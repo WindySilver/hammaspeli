3c865d52ff04dfa468280162080b624fe91c226f
WindySilver <59417082+WindySilver@users.noreply.github.com>
2023-02-04 12:30:35 +0200

    Add movement controls for WASD and update gitignore

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -22,10 +22,10 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    if (Input.GetKey(KeyCode.RightArrow)){
+	    if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.LeftArrow)){
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
 			transform.position += Vector3.left* speed * Time.deltaTime;
 		}
 		
