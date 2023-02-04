1d8f75cacd564e48a189819d0b9823c059627784
Triffro <mikael.krats@gmail.com>
2023-02-04 16:06:59 +0200

    crumpling platform script

---
+++ Assets/Scripts/CrumplingPlatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumplingPlatform : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"));
+        StartCoroutine(destroyDelay());
+    }
+    
+    private IEnumerator destroyDelay()
+    {
+        yield return new WaitForSeconds(3);
+        StartCoroutine(spawnDelay());
+    }
+    
+    private IEnumerator spawnDelay()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+            //material.color.a = 1; SetActive(false);
+        yield return new WaitForSeconds(5);
+        gameObject.GetComponent<Renderer>().enabled = true;
+    }
+}
